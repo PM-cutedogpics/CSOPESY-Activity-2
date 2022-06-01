4ba0a26bda3f69221d7cc28f3f3f2222aa270218
Ben <benedict_sun@dlsu.edu.ph>
2022-06-01 17:24:24 +0800

    Code cleanup

--- Philosopher.java
+++ Philosopher.java
@@ -1,13 +1,11 @@
 import java.util.Random;
 
 public class Philosopher extends Thread{
-    String state;
     int id;
     DiningPhilosopher dp;
     Random rng;
 
     public Philosopher(int id, DiningPhilosopher dp) {
-        this.state = "THINKING";
         this.id = id;
         this.dp = dp;
         this.rng = new Random();
@@ -18,19 +16,18 @@ public class Philosopher extends Thread{
      */
     @Override
     public void run() {
+        int min = 3000;
+        int max = 10000;
         try{
-            //
             while (true) {
                 // Back to thinking
                 System.out.println("Philosopher " + id + " is thinking.");
-                //Thread.sleep(rng.nextInt(5000 - 1000 + 1) + 1000);
-                Thread.sleep(2000);
+                Thread.sleep(rng.nextInt(max - min + 1) + min);
 
                 // Try to pickup and eat
                 dp.pickup(id);
                 System.out.println("Philosopher " + id + " is eating.");
-                //Thread.sleep(rng.nextInt(5000 - 1000 + 1) + 1000);
-                Thread.sleep(2000);
+                Thread.sleep(rng.nextInt(max - min + 1) + min);
 
                 // Drop
                 dp.putdown(id);
